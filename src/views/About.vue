f2ad3c31607fe636438673ee426d3b3dead4c496
20295 <dapeng20>
2020-06-15 00:23:01 +0800

    毕业设计完整版（前端）

---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <s-header :name="'关于我们'"></s-header>
+    <div class="about-body">
+      新蜂商城
+    </div>
+  </div>
+</template>
+
+<script>
+import sHeader from '@/components/SimpleHeader'
+export default {
+  components: {
+    sHeader
+  },
+}
+</script>
+
+<style lang="less" scoped>
+  .about {
+    .about-body {
+      margin-top: 44px;
+    }
+  }
+</style>
